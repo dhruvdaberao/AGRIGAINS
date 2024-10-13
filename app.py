453801f5e50fa7fba73b9ac568363a2e238da608
dhruvdaberao <dhruvdaberao777@gmail.com>
2024-10-13 18:06:06 +0530

    changed app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, Response, jsonify
 import numpy as np
 import pandas as pd
 import pickle, joblib
-import ast
+import ast, os
 
 # importing model
 model = pickle.load(open('./static/model/model.pkl', 'rb'))
@@ -162,4 +162,5 @@ def pYield():
 
 # python main
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
